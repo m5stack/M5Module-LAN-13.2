dcfebb4b079050870a6ea928665db4f70d2fb869
sean <1026025056@qq.com>
2024-01-25 11:27:19 +0800

    update examples

--- examples/HTTP/HTTP.ino
+++ examples/HTTP/HTTP.ino
@@ -90,19 +90,6 @@ void setup() {
         Serial.println("Error getting IP address via DHCP, trying again...");
         delay(2000);
     }
-
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println(
-            "Ethernet shield was not found.  Sorry, can't run without "
-            "hardware. :(");
-        while (true) {
-            delay(1);  // do nothing, no point running without Ethernet hardware
-        }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println("Ethernet cable is not connected.");
-    }
 }
 
 void loop() {
--- examples/WebServer/WebServer.ino
+++ examples/WebServer/WebServer.ino
@@ -78,19 +78,6 @@ void setup() {
         delay(2000);
     }
 
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println(
-            "Ethernet shield was not found.  Sorry, can't run without "
-            "hardware. :(");
-        while (true) {
-            delay(1);  // do nothing, no point running without Ethernet hardware
-        }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println("Ethernet cable is not connected.");
-    }
-
     // start the server
     server.begin();
     Serial.print("server is at ");
